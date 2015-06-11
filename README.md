410aa75222af494fa095955c8ffc8d1b006c791f
Panisuan Chasinga <jo.chasinga@gmail.com>
2015-06-10 23:11:45 -0400

    Add PortUtil

--- README.md
+++ README.md
@@ -1,3 +1,74 @@
-# pluto
-The One and Only Python Arduino Framework
+pluto
+=====
+The one and only Arduino framework in Python. It is built on top of [pyFirmata](https://github.com/tino/pyFirmata).
+
+Intention
+=========
++ A high-level Python interface to microcontrollers like the Arduinos.
++ Use asynchronous non-block I/O
+
+Test status:
+
+Untested (still in alpha).
+Only tried on an Arduino Uno and Mac OSX 10.9.5.
+
+Installation
+============
+
+```bash
+
+git lcone https://github.com/jochasinga/pluto
+cd pluto
+python setup.py install
+
+```
+
+.. _setuptools: https://pypi.python.org/pypi/setuptools
+
+Examples
+========
+
+Control on-board LED
+--------------------
+
+    >>> from pluto import *
+    >>> board = Uno('/dev/cu.usbmodem1411')
+    >>> board.led.on()
+    >>> board.led.off()
+
+*pluto* has `PortUtil` class which use *regex* to search for a relevant port.
+
+    >>> PortUtil.scan()  # -> '/dev/cu.usbmodem1411'
+
+This makes it possible to create an instance of the board without explicitly providing the port name.
+
+    >>> board = Board()
+    >>> board.led.blink()
+
+`ArduinoUtil` is instantiated with the board so Arduino C-style APIs can be used.
+
+    >>> board = Board()
+    >>> board.digitalWrite(13, HIGH)
+    >>> board.digitalWrite(13, LOW)
+
+It's not Pythonic, and I'm considering replacing camelCase with underscore.
+
+TODO
+====
+
+It's just a start, and there's a lot more to do. Here is a few ideas:
+
++ Implement PWM and analog functionalities.
++ Optimize the code and divide into several modules.
++ Make *pluto* a fork of *pyFirmata* instead of relying on it.
+
+
+
+
+
+
+
+
+
+
 
--- pluto/pluto.py
+++ pluto/pluto.py
@@ -1,7 +1,6 @@
 from pyfirmata import *
 from boards import BOARDS
-import pyfirmata
-# import time
+from ports import *
 import gevent
 
 __version__ = '0.1.0'
@@ -34,6 +33,77 @@ class ArduinoUtil(object):
                 board.digital[pin_number].write(LOW)
                 gevent.sleep(1)
 
+class Board(pyfirmata.Board):
+    """The Base class for any board."""
+    def __init__(self, port=None, layout=None, baudrate=57600, name=None, timeout=None):
+        self.util = ArduinoUtil()
+        self.led = Led(self)
+        self.pin = Pin(self)
+        auto_port = PortUtil.scan()
+        self.sp = serial.Serial(auto_port, baudrate, timeout=timeout)
+        # Allow 5 secs for Arduino's auto-reset to happen
+        # Alas, Firmata blinks its version before printing it to serial
+        # For 2.3, even 5 seconds might not be enough.
+        # TODO Find a more reliable way to wait until the board is ready
+        self.pass_time(BOARD_SETUP_WAIT_TIME)
+        self.name = name
+        self._layout = layout
+        if not self.name:
+            self.name = port
+
+        if layout:
+            self.setup_layout(layout)
+        else:
+            self.auto_setup()
+
+        # Iterate over the first messages to get firmware data
+        while self.bytes_available():
+            self.iterate()
+        # TODO Test whether we got a firmware name and version, otherwise there
+        # probably isn't any Firmata installed
+
+    def digitalWrite(self, pin_number, value):
+        self.util.digitalWrite(self, pin_number, value)
+
+    def digitalRead(self):
+        self.util.digitalRead(self)
+
+    def blinkLED(self, pin_number=13, interval=1):
+        self.util.blinkLED(self)
+
+    def at_pin(self, pin_number):
+        self.pin = Pin(self, pin_number)
+                
+class Pin(pyfirmata.Pin):
+    """Pluto's Pin representation"""
+    def __init__(self, board, pin_number=LED_BUILTIN, type=ANALOG, port=None):
+        super(Pin, self).__init__(board, pin_number, type, port)
+        self.util = ArduinoUtil()
+        
+    def digitalWrite(self, *args, **kwargs):
+        for index, val in enumerate(args):
+            if index == 0:
+                self.pin_number = val
+            elif index == 1:
+                self.value = val
+
+        if kwargs is not None:
+            for key, val in kwargs.iteritems():
+                if key == "pin_number":
+                    self.pin_number = val
+                elif key == "value":
+                    self.value = val
+                else:
+                    pass
+
+        self.util.digitalWrite(self.board, self.pin_number, self.value)
+
+    def high(self):
+        self.digitalWrite(pin_number=self.pin_number, value=HIGH)
+
+    def low(self):
+        self.digitalWrite(pin_number=self.pin_number, value=LOW)
+                
 class Led(Pin):
     """Led representation"""    
     def __init__(self, board, pin_number=LED_BUILTIN, type=ANALOG, port=None):
@@ -53,33 +123,6 @@ class Led(Pin):
                 self.board.digitalWrite(self.pin_number, LOW)
                 gevent.sleep(interval)
 
-
-class Pin(pyfirmata.Pin):
-    """Pluto's Pin representation"""
-    def __init__(self, board, pin_number=LED_BUILTIN, type=ANALOG, port=None):
-        super(Pin, self).__init__(board, pin_number, type, port)
-        self.util = ArduinoUtil()
-
-    def digitalWrite(self, *args, **kwargs):
-
-        for index, val in enumerate(args):
-            if index == 0:
-                self.pin_number = val
-            elif index == 1:
-                self.value = val
-
-        if kwargs is not None:
-            for key, val in kwargs.iteritems():
-                if key == "pin_number":
-                    self.pin_number = val
-                elif key == "value":
-                    self.value = val
-                else:
-                    pass
-
-        self.util.digitalWrite(self.board, self.pin_number, self.value)
-
-# wrapper classes
 class Uno(Board):
     """
     A board that will set itself up as an Arduino Uno
@@ -87,18 +130,6 @@ class Uno(Board):
     def __init__(self, *args, **kwargs):
         super(Uno, self).__init__(*args, **kwargs)
         self.name = 'arduino_uno'
-        self.util = ArduinoUtil()
-        self.led = Led(self)
-        self.pin = Pin(self)
-
-    def digitalWrite(self, pin_number, value):
-        self.util.digitalWrite(self, pin_number, value)
-
-    def digitalRead(self):
-        self.util.digitalRead(self)
-
-    def blinkLED(self, pin_number=13, interval=1):
-        self.util.blinkLED(self)
 
     def __str__(self):
         super(Uno, self).__str__()
