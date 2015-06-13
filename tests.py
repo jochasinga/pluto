1b436eb5bb9e5cb5ac0bded0e576198629d0a5aa
Panisuan Chasinga <jo.chasinga@gmail.com>
2015-06-13 18:54:21 -0400

    Add more tests

--- tests.py
+++ tests.py
@@ -4,14 +4,14 @@ import sys, os
 import unittest
 import serial
 import pyfirmata
+import re
+import serial.tools.list_ports
 from pyfirmata import mockup
 
 sys.path.append('./pluto')
 import pluto
 from boards import BOARDS
 from utils import ArduinoUtil, PortUtil
-
-TEST_PORT = '/dev/cu.usbmodem1411'
 
 class BoardBaseTest(unittest.TestCase):
     def setUp(self):
@@ -23,19 +23,33 @@ class BoardInitTest(BoardBaseTest):
     def test_board_initiated(self):
         self.assertIsInstance(self.board, pluto.Board)
 
-class UtilityTest(unittest.TestCase):
+    def test_board_led(self):
+        self.assertEqual(self.board.led.on(), self.board.digital[13].write(1))
+        self.assertEqual(self.board.led.off(), self.board.digital[13].write(0))
+        # TODO: Test blink method
+        # TODO: Test strope method
+
+class PortUtilityTest(unittest.TestCase):
     def test_auto_port_count(self):
-        self.assertIsNotNone(PortUtil.count_ports())
+        self.comports = [p[0] for p in serial.tools.list_ports.comports()]
+        self.assertEqual(PortUtil.count_ports(), len(self.comports))
     
     def test_auto_port_discovery(self):
-        self.assertEqual(PortUtil.scan(), TEST_PORT)
+        # TODO: Add new portnames to the list
+        TEST_PORTS = [
+            '/dev/cu.usbmodem1411',
+            '/dev/tty.usbserial4321',
+            '/dev/cu.usbbabe999',
+            '/dev/tty.serialport666',
+            '/dev/abc.usbrocks12'
+        ]
 
-    def test_arduino_utility(self):
-        # self.util.digitalWrite(self.board, 13, HIGH)
-        pass
+        for port in TEST_PORTS:
+            self.assertIn(PortUtil.scan(), TEST_PORTS)
 
 class PinTest(BoardBaseTest):
     def test_pin_initiated(self):
+        # TODO: Use MockPin
         self.pin = pluto.Pin(self.board)
         self.assertIsInstance(self.pin, pluto.Pin)
 
