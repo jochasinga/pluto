6e32eb02f352e66517b6cc7eab1e611d0a0537b5
Panisuan Chasinga <jo.chasinga@gmail.com>
2015-06-13 11:38:59 -0400

    Add setup

--- setup.py
+++ setup.py
@@ -17,6 +17,8 @@ setup(
     install_requires=['pyserial', 'pyfirmata'],
     zip_safe=False,
     url='https://github.com/jochasinga/pluto',
+    download_url = 'https://github.com/jochasinga/pluto/tarball/0.1.0',
+    keywords = ['arduino', 'robotics', 'diy', 'iot', 'electronics'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Other Environment',
